715acdbdf4843dd687281765067a8e59a39cc6e6
PJ Frias <peejfrias@gmail.com>
2018-03-06 13:30:42 +0000

    save

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,3 +1,19 @@
+require 'pry'
 class LineItemsController < ApplicationController
 
+  def create
+    if current_user.current_cart == nil
+      new_cart = Cart.create
+      current_user.current_cart = new_cart
+      current_user.save
+      current_user.current_cart.save
+      binding.pry
+    end
+    line_item = current_user.current_cart.add_item(params[:item_id])
+    if line_item.save
+      redirect_to cart_path(current_user.current_cart), {notice: 'Item added to cart!'}
+    else
+      redirect_to store_path, {notice: 'Unable to add item'}
+    end
+  end
 end
